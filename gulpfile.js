dcac30529fdb083df4161f2136ad8243ba4ed4a7
Chiheb Bouzid <chihebouzid@gmail.com>
2019-02-23 12:05:57 +0100

    make it better

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const htmlclean = require('gulp-htmlclean');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+
 
 
 //Compile Sass
@@ -9,6 +14,32 @@ gulp.task('sass', function () {
         .pipe(sass())
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.stream());
+});
+
+// Copy All HTML files
+gulp.task('copyHtml', function () {
+    gulp.src('src/*.html')
+        .pipe(htmlclean())
+        .pipe(gulp.dest('dist'));
+});
+
+
+
+//clean and build css files
+gulp.task('buildCss', function () {
+    return gulp.src('src/css/*.css')
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dist/css'));
+});
+
+
+//clean and build js files
+gulp.task('buildJs', function () {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('script.min.js'))
+        .pipe(terser())
+        .pipe(gulp.dest('dist/js'));
 });
 
 
@@ -22,7 +53,6 @@ gulp.task('serve', gulp.series('sass', function () {
     gulp.watch(['src/*.html']).on('change', browserSync.reload);
 }));
 
-
 //Default
 
-gulp.task('default', gulp.series('serve'));
+gulp.task('default', gulp.parallel('serve', 'copyHtml', 'buildCss', 'buildJs'));
